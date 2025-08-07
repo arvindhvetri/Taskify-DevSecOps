d1eb675e1d45fe2f9936d9ca1f10ff4da69891c0
Arvindh Kumar V <arvindhsharmi@gmail.com>
2025-08-07 21:03:37 +0530

    Create localhost.sh

---
+++ Ansible-Playbook/localhost.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+sudo apt install -y openjdk-17-jdk
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
